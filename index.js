a3bee0b2f2d4582b428c76ca71d2b15d2d7f2361
Mira Chami <miramc149@gmail.com>
2021-04-14 12:00:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+    return (`Wake ${dogName} the ${dogBreed}`);
 }
 function leashDog(dogName, dogBreed) {
-  console.log(`Leach ${dogName} the ${dogBreed}`);
+  return (`Leash ${dogName} the ${dogBreed}`);
 }
 function walkToPark(dogName, dogBreed) {
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return (`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return (`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 function walkHome(dogName, dogBreed) {
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return (`Walk home with ${dogName} the ${dogBreed}`);
 }
 function unleashDog(dogName, dogBreed) {
-  console.log(`Unleach ${dogName} the ${dogBreed}`);
+  return (`Unleash ${dogName} the ${dogBreed}`);
 }
-  routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
-function exerciseDog(dogName, dogBreed){
-  for(let i=o;i<routine.length;i++){
-
-    return routine[i](dogName, dogBreed);
-  }
+var routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+function exerciseDog(dog, breed) {
+  return routine.map(fn => fn(dog, breed))
 }
